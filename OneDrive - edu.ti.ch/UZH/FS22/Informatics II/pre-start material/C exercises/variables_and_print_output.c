d9024994c60c71672252e69cc87b0a4aa24ed6db
Mondra <jerryscout71@gmail.com>
2022-01-07 10:56:59 +0100

    first commit

---
+++ OneDrive - edu.ti.ch/UZH/FS22/Informatics II/pre-start material/C exercises/variables_and_print_output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int age = 25;
+    printf("Age: %d", age);
+    age = 31;
+    printf("\nNew Age: %d", age);
+    int firstNumber = 33;
+    printf("\nFirst Number = %d", firstNumber);
+    int secondNumber = firstNumber;
+    printf("\nSecond Number = %d", secondNumber);
+    int variable1 = 14, variable2 = 25;
+    printf("\nFirst Variable = %d", variable1);
+    return 0;
+}
